c10cb08fcf5d7267d2d968f2c1618bb14996c7ba
Mercy <mercymumbe17@gmail.com>
2025-05-15 00:44:53 +0300

    initialized class Anagram with init and match classes

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        return [
+            candidate for candidate in word_list
+            if sorted(candidate.lower()) == sorted(self.word)
+            and candidate.lower() != self.word
+        ]
+
